c278d93a7df8810375ac390832a0120f2a3d34f3
Raheel2004 <rafiqueshaikh200416@gmail.com>
2025-08-07 17:46:50 +0600

    Update salary_prediction_using_traditional_ml_techniques_py (1).py

--- salary_prediction_using_traditional_ml_techniques_py (1).py
+++ salary_prediction_using_traditional_ml_techniques_py (1).py
@@ -10,7 +10,7 @@ Original file is located at
 # Step 1: Import Libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
