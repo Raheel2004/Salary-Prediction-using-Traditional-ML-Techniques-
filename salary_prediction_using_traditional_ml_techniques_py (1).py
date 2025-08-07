d4053a0460ba4cdb58fe5fcf9c09534aa582a0cc
Raheel2004 <rafiqueshaikh200416@gmail.com>
2025-08-07 17:48:33 +0600

    Update salary_prediction_using_traditional_ml_techniques_py (1).py

--- salary_prediction_using_traditional_ml_techniques_py (1).py
+++ salary_prediction_using_traditional_ml_techniques_py (1).py
@@ -9,12 +9,7 @@ Original file is located at
 
 # Step 1: Import Libraries
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-import streamlit as st
+
 
 
 #1. Choose a Dataset
